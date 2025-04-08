da43d4a3b535d74f6cfd75975d2abe6f95160df0
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-04-08 00:16:54 -0400

    File updated at 4/8/2025, 12:16:54 AM

--- 198-HouseRobber/198-HouseRobber.py
+++ 198-HouseRobber/198-HouseRobber.py
@@ -1,18 +1,18 @@
-# Last updated: 4/7/2025, 10:27:13 PM
+# Last updated: 4/8/2025, 12:16:54 AM
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 0:
-            return 0
-        if n == 1:
-            return nums[0]
-        dp = [-1] * n
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2,n):
-            left = dp[i-1]
-            right = nums[i] + dp[i-2]
-            dp[i] = max(left,right)
-        return dp[n- 1]
+        prev = nums[0]
+        prev2 = 0
+
+        for i in range(1,n):
+            pick = nums[i]
+            if i > 1:
+                pick += prev2
+            notpick = prev
+            curr = max(pick , notpick)
+            prev2 = prev
+            prev = curr
+        return prev
         
         
