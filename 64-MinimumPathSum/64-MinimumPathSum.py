c4cf29a6d01912facbd15ad9b17d5cae55022568
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-04-10 17:02:04 -0400

    File updated at 4/10/2025, 5:02:07 PM

--- 64-MinimumPathSum/64-MinimumPathSum.py
+++ 64-MinimumPathSum/64-MinimumPathSum.py
@@ -1,26 +1,28 @@
-# Last updated: 4/10/2025, 5:00:14 PM
+# Last updated: 4/10/2025, 5:02:07 PM
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        dp = [[0 for _ in range(m)] for _ in range(n)]
+        prev = [0] * m
         for i in range(n):
+            temp = [0] * m
             for j in range(m):
                 if i == 0 and j == 0:
-                    dp[i][j] = grid[0][0]
+                    temp[j] = grid[0][0]
                 else:
                     up = grid[i][j]
                     if i > 0:
-                        up += dp[i-1][j]
+                        up += prev[j]
                     else:
                         up += float('inf')
                     left = grid[i][j]
                     if j >0:
-                        left += dp[i][j-1]
+                        left += temp[j-1]
                     else:
                         left += float('inf')
-                    dp[i][j] = min(left,up)
+                    temp[j] = min(left,up)
+            prev = temp
 
-        return dp[n-1][m-1] 
+        return prev[m-1] 
     
         
