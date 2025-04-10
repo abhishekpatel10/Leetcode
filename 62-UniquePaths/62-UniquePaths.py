397bd4433c2b901605ddb2af1b1422cbc68be7c8
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-04-10 01:25:52 -0400

    Create file

---
+++ 62-UniquePaths/62-UniquePaths.py
@@ -0,0 +1,18 @@
+# Last updated: 4/10/2025, 1:25:55 AM
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[-1 for _ in range(n)]for _ in range(m)]
+        return self.solve(m-1,n-1,dp)
+    def solve(self,i,j,dp):
+        if i == 0 and j == 0:
+            return 1
+        if i < 0 or j < 0 :
+            return 0
+        if dp[i][j] != -1:
+            return dp[i][j]
+        up = self.solve(i-1,j,dp)
+        left = self.solve(i,j-1,dp)
+        dp[i][j] = up + left
+        return dp[i][j]
+
+        
