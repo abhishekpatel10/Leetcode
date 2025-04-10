f6c6ae7cdf092f0cd01f78081da931af0c3df8d4
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-04-10 16:11:30 -0400

    File updated at 4/10/2025, 4:11:33 PM

--- 63-UniquePathsII/63-UniquePathsII.py
+++ 63-UniquePathsII/63-UniquePathsII.py
@@ -1,26 +1,28 @@
-# Last updated: 4/10/2025, 4:08:18 PM
+# Last updated: 4/10/2025, 4:11:33 PM
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         n = len(obstacleGrid)
         m = len(obstacleGrid[0])
-        dp = [[0 for _ in range(m) ] for _ in range(n)]
+        prev = [0] *m
         if obstacleGrid[0][0] == 1:
             return 0
         for i in range(n):
+            temp = [0] * m
             for j in range(m):
                 if obstacleGrid[i][j] == 1:
-                    dp[i][j] = 0
+                    temp[j] = 0
                 elif i == 0 and j == 0:
-                    dp[i][j] = 1
+                    temp[j] = 1
                 else:
                     up = 0
                     if i > 0:
-                        up += dp[i-1][j]
+                        up += prev[j]
                     left = 0
                     if j > 0:
-                        left += dp[i][j-1]
-                    dp[i][j] = up + left
-        return dp[n-1][m-1]
+                        left += temp[j-1]
+                    temp[j] = up + left
+            prev = temp
+        return prev[m-1]
 
             
         
