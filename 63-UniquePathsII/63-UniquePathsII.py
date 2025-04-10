07240e93e39f523469ca4d43e613dd2b54a9ddd6
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-04-10 16:08:14 -0400

    File updated at 4/10/2025, 4:08:18 PM

--- 63-UniquePathsII/63-UniquePathsII.py
+++ 63-UniquePathsII/63-UniquePathsII.py
@@ -1,28 +1,26 @@
-# Last updated: 4/10/2025, 3:44:52 PM
+# Last updated: 4/10/2025, 4:08:18 PM
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         n = len(obstacleGrid)
         m = len(obstacleGrid[0])
-        dp = [[-1 for _ in range(m) ] for _ in range(n)]
+        dp = [[0 for _ in range(m) ] for _ in range(n)]
         if obstacleGrid[0][0] == 1:
             return 0
+        for i in range(n):
+            for j in range(m):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                elif i == 0 and j == 0:
+                    dp[i][j] = 1
+                else:
+                    up = 0
+                    if i > 0:
+                        up += dp[i-1][j]
+                    left = 0
+                    if j > 0:
+                        left += dp[i][j-1]
+                    dp[i][j] = up + left
+        return dp[n-1][m-1]
+
             
-        return self.solve(n-1,m-1,obstacleGrid,dp)
         
-    def solve(self,i,j,obstacleGrid,dp):
-        if i ==0 and j == 0 :
-            return 1
-        if i < 0 or j < 0:
-            return 0
-        if obstacleGrid[i][j] == 1:
-            return 0
-        if dp[i][j] != -1:
-            return dp[i][j]
-        up = 0
-        if i > 0:
-            up += self.solve(i-1,j,obstacleGrid,dp)
-        left = 0
-        if j > 0:
-            left += self.solve(i,j-1,obstacleGrid,dp)
-        dp[i][j] = up + left
-        return dp[i][j]
