7d34736ef3367c95abc9f8e0c945e31546ae0d9b
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-04-11 01:22:47 -0400

    File updated at 4/11/2025, 1:22:51 AM

--- 931-MinimumFallingPathSum/931-MinimumFallingPathSum.py
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.py
@@ -1,27 +1,29 @@
-# Last updated: 4/11/2025, 1:20:04 AM
+# Last updated: 4/11/2025, 1:22:51 AM
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
         curr_min = float('inf')
-        dp = [[0 for _ in range(n)]for _ in range(n)]
+        prev = [0] * n
         for i in range(n):
-            dp[0][i] = matrix[0][i]
+            prev[i] = matrix[0][i]
         for i in range(1,n):
+            temp = [0] * n
             for j in range(n):
-                up = matrix[i][j] + dp[i-1][j]
+                up = matrix[i][j] + prev[j]
                 
                 downleft = matrix[i][j]
                 if j - 1 >= 0:
-                    downleft += dp[i-1][j-1]
+                    downleft += prev[j-1]
                 else:
                     downleft += float('inf') 
                 downright = matrix[i][j]
                 if j+1 <n:
                     
-                    downright += dp[i-1][j+1]
+                    downright += prev[j+1]
                 else:
                     downright += float('inf')
-                dp[i][j] = min(up,downleft,downright)
+                temp[j] = min(up,downleft,downright)
+            prev = temp
+        return min(prev)
                 
 
-        return min(dp[n-1])
