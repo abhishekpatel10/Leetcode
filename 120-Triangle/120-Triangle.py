b5e4c01243885836af5e6774d89cca18bbc95c9c
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-04-11 00:33:13 -0400

    File updated at 4/11/2025, 12:33:17 AM

--- 120-Triangle/120-Triangle.py
+++ 120-Triangle/120-Triangle.py
@@ -1,21 +1,21 @@
-# Last updated: 4/11/2025, 12:17:55 AM
+# Last updated: 4/11/2025, 12:33:17 AM
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        m = len(triangle[n - 1])
-        dp = [[-1 for _ in range(m)] for _ in range(n)]
+        prev = [0]*n
+        for i in range(n):
+            prev[i] = triangle[n-1][i]
+        for i in range(n-2,-1,-1):
+            temp = [0] * n
+            for j in range(i,-1,-1):
+                down = triangle[i][j] + prev[j]
+                diag = triangle[i][j] + prev[j+1]
+                temp[j] = min(down,diag)
+            prev = temp
+            
+        return prev[0]
         
 
-        return self.solve(triangle,0,0,n,dp)
-    
-    def solve(self,triangle,i,j,n,dp):
-        if i == n-1:
-            return triangle[i][j]
-        if dp[i][j] != -1:
-            return dp[i][j]
-        down = triangle[i][j] + self.solve(triangle,i+1,j,n,dp)
-        diag = triangle[i][j] + self.solve(triangle,i+1,j+1,n,dp)
-        dp[i][j] = min(down,diag)
-        return dp[i][j]
+       
     
         
