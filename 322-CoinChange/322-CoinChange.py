370fda93eebc12cafe85ed7ec741ea909736edef
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-04-13 17:48:35 -0400

    File updated at 4/13/2025, 5:48:36 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,18 +1,13 @@
-# Last updated: 4/13/2025, 5:45:43 PM
+# Last updated: 4/13/2025, 5:48:36 PM
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
-        dp = [[float('inf') for _ in range(amount +1)] for _ in range(n)]
-        for j in range(amount + 1):
-            if j % coins[0] == 0:
-                dp[0][j] = j // coins[0]
-        for i in range(1,n):
-            for j in range(amount +1):
-                nottaken = dp[i - 1][j]
-                taken = float('inf')
-                if coins[i] <= amount:
-                    taken = 1 + dp[i][j - coins[i]]
-                dp[i][j] = min(taken,nottaken)
-        result = dp[n-1][amount]
-        return -1 if result == float('inf') else int(result)
-    
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        #array indx
+        for i in range(1, amount + 1):
+            for c in coins:
+                if (i - c) >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - c])
+
+        return dp[amount] if (dp[amount] != amount + 1) else -1
