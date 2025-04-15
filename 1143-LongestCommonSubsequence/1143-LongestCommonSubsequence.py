f1eae0592dfeac2e050504313fb776d62e9d5fd0
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-04-15 02:55:32 -0400

    File updated at 4/15/2025, 2:55:32 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.py
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.py
@@ -1,17 +1,15 @@
-# Last updated: 4/15/2025, 2:39:37 AM
+# Last updated: 4/15/2025, 2:55:32 AM
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         m = len(text1)
         n = len(text2)
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
 
-        return self.solve(m-1,n-1,text1,text2,dp)
-    def solve(self,m,n,text1,text2,dp):
-        if m < 0 or n < 0:
-            return 0
-        if dp[m][n] != -1:
-            return dp[m][n]
-        if text1[m] == text2[n]:
-            return 1 + self.solve(m-1,n-1,text1,text2,dp)
-        dp[m][n] = max(self.solve(m-1,n,text1,text2,dp),self.solve(m,n-1,text1,text2,dp))
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
         return dp[m][n]
+        
