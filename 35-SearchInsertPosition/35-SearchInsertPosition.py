fdfb6db233fcb30a189b16c24822747c84fca111
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-05-19 16:11:49 -0400

    File updated at 5/19/2025, 4:11:52 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.py
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -1,10 +1,10 @@
-# Last updated: 5/4/2025, 8:52:43 PM
+# Last updated: 5/19/2025, 4:11:52 PM
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         l = 0
         r = len(nums) - 1
         m = 0
-        ans = target
+        ans = len(nums)
         while l <=r:
             m = (r+l) // 2
             if nums[m] >= target:
@@ -12,7 +12,7 @@ class Solution:
                 r = m -1
             else:
                 l = m + 1
-        return l
+        return ans
                 
         
         
