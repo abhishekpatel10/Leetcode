b0768b4fac8b047adb9b77a9fc6e89cc8c1078d7
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-08 11:52:51 -0500

    Create 1335-MaximumCandiesAllocatedToKChildren

---
+++ 1335-MaximumCandiesAllocatedToKChildren/1335-MaximumCandiesAllocatedToKChildren.py
@@ -0,0 +1,18 @@
+# Last updated: 6/8/2025, 11:52:57 AM
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        left, right = 1, max(candies)
+        result = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            children_count = sum(pile // mid for pile in candies)
+            
+            if children_count >= k:
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return result
