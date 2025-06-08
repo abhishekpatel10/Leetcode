af425c95b1adb953102f0d5acd4d44049a6b5abb
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-08 11:52:31 -0500

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.java
@@ -0,0 +1,20 @@
+// Last updated: 6/8/2025, 11:52:37 AM
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while(i < word1.length() || i <word2.length()){
+            if(i < word1.length()){
+                sb.append(word1.charAt(i));
+            }
+            if(i < word2.length()){
+                sb.append(word2.charAt(i));
+            }
+            i++;
+        }
+
+        return sb.toString();
+
+        
+    }
+}
