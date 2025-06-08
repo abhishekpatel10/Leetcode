2861f39404573387bc7ce7a4577d3529f5f0cae3
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-08 11:54:14 -0500

    Create 386-LexicographicalNumbers

---
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.py
@@ -0,0 +1,23 @@
+# Last updated: 6/8/2025, 11:54:19 AM
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        self.ans = []
+
+        def solve(i):
+            # base conditon - where recursion gets over
+            if i > n:
+                return
+
+            cur = i  # 1 
+            self.ans.append(cur)
+            for i in range(10):
+                tmp = str(cur) + str(i)  # 1 0 
+                solve(int(tmp))
+
+                # int tmp = cur*10 +  i;
+                # solve(tmp,n);
+
+        for i in range(1, 10):
+            solve(i)
+
+        return self.ans
