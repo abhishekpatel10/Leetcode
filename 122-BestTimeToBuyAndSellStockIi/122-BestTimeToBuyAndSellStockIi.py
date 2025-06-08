4a6b6de2f9402c2caa041793e1b40e05a3f789b6
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-08 11:55:05 -0500

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.py
@@ -0,0 +1,22 @@
+# Last updated: 6/8/2025, 11:55:11 AM
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        summ = 0
+        i = 0
+        n = len(prices)
+        hi = prices[0]
+        lo = prices[0]
+
+        while i < n- 1:
+            while i < n -1 and prices[i] >=  prices[i+1]:
+                i+=1
+            lo = prices[i]
+
+            while i < n-1 and prices[i] <= prices[i +1]:
+                i +=1
+            hi = prices[i]
+            summ += hi - lo
+        return summ
+
+            
+
