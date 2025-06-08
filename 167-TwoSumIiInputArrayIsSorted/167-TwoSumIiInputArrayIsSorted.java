2bb6a35a81765f6d9785b5d89d7e01005aaafb12
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-08 11:54:48 -0500

    Create 167-TwoSumIiInputArrayIsSorted

---
+++ 167-TwoSumIiInputArrayIsSorted/167-TwoSumIiInputArrayIsSorted.java
@@ -0,0 +1,22 @@
+// Last updated: 6/8/2025, 11:54:54 AM
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start < end){
+            int sum = numbers[start] + numbers[end];
+            if(sum == target){
+                return new int[]{start + 1 , end +1};
+            }else if(sum < target){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
+        return new  int[]{-1,-1};
+
+        
+    }
+}
