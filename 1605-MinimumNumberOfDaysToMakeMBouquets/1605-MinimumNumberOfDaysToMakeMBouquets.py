92fd10564aec26aa9dfe5ca4281cd9c0c6d3767e
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-08 11:52:39 -0500

    Create 1605-MinimumNumberOfDaysToMakeMBouquets

---
+++ 1605-MinimumNumberOfDaysToMakeMBouquets/1605-MinimumNumberOfDaysToMakeMBouquets.py
@@ -0,0 +1,38 @@
+# Last updated: 6/8/2025, 11:52:45 AM
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        def func(bloomDay,mid,m,k):
+            cnt = 0
+            ans = 0
+            for b in bloomDay:
+                if b <= mid :
+                    cnt += 1
+                else:
+                    ans += floor(cnt/k)
+                    cnt = 0
+            ans += floor(cnt/k)
+            if ans >= m:
+                return True
+            else:
+                return False
+        n = len(bloomDay)
+        totalflowers = m * k
+        if n < m * k:
+            return -1
+        
+        l = min(bloomDay)
+        r = max(bloomDay)
+        ans = 0
+        while l <= r:
+            mid = (r+l) // 2
+            total_min = func(bloomDay,mid,m,k)
+            if total_min:
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        if ans > 0:
+            return ans
+        else:
+            return -1
+
