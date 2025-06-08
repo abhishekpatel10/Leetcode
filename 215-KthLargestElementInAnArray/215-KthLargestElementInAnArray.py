79f0d9524248b5f570dbc282060f1c3e6d38317d
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-08 11:54:38 -0500

    Create 215-KthLargestElementInAnArray

---
+++ 215-KthLargestElementInAnArray/215-KthLargestElementInAnArray.py
@@ -0,0 +1,12 @@
+# Last updated: 6/8/2025, 11:54:44 AM
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        pq = []
+        n = len(nums)
+        for i in range(n):
+            heapq.heappush(pq,-nums[i])
+        f = k - 1
+        while f > 0:
+            heapq.heappop(pq)
+            f-=1
+        return -pq[0]
