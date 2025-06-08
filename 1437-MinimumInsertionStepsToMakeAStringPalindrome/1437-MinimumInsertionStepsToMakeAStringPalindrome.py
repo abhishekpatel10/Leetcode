bd7ee3afc722d0fb6b7b0245325c0742c0d67e4c
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-08 11:52:49 -0500

    Create 1437-MinimumInsertionStepsToMakeAStringPalindrome

---
+++ 1437-MinimumInsertionStepsToMakeAStringPalindrome/1437-MinimumInsertionStepsToMakeAStringPalindrome.py
@@ -0,0 +1,17 @@
+# Last updated: 6/8/2025, 11:52:54 AM
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        s1 = s[::-1]
+        m = len(s)
+        n = len(s1)
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[j-1] == s1[i-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1])
+        
+        final_ans = m - dp[n][m]
+        return final_ans
+    
