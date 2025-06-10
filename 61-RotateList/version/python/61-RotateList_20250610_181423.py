b5b80cf5681770b3c5f2f8a528b04a90078564ad
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-10 18:14:22 -0500

    Create 61-RotateList

---
+++ 61-RotateList/version/python/61-RotateList_20250610_181423.py
@@ -0,0 +1,26 @@
+# Last updated: 6/10/2025, 6:14:23 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0 :
+                return head
+        curr = head
+        length = 1
+        while curr.next:
+            curr = curr.next
+            length += 1
+        curr.next = head
+
+        k = length -(k%length)
+        while k:
+            curr = curr.next
+            k -=1
+        newHead = curr.next
+        curr.next = None
+        return newHead
+
+            
