4188cbbdbf43c52e2f5a303b30cd9f95bf3be5c4
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-06-30 16:51:53 -0500

    Create 127-WordLadder

---
+++ 127-WordLadder/version/python/127-WordLadder_20250630_165153.py
@@ -0,0 +1,22 @@
+# Last updated: 6/30/2025, 4:51:53 PM
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        q = deque()
+        s = set(wordList)
+        q.append((beginWord,1))
+        while q:
+            curr_word , steps = q.popleft()
+            if curr_word == endWord:
+                return steps
+            for i in range(len(curr_word)):
+                for ch in range(97,123):
+                    if chr(ch) == curr_word[i]:
+                        continue
+                    nei = curr_word[:i] + chr(ch) + curr_word[i+1:]
+                    if nei in s:
+                        q.append((nei,steps+1))
+                        s.remove(nei)
+        
+        return 0
