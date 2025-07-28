b81f0392029645c452d8406c21039d91c32f064c
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-07-28 18:04:24 -0500

    Create 1781-SumofBeautyofAllSubstrings

---
+++ 1781-SumofBeautyofAllSubstrings/version/python/1781-SumofBeautyofAllSubstrings_20250728_180426.py
@@ -0,0 +1,10 @@
+# Last updated: 7/28/2025, 6:04:26 PM
+class Solution:
+    def beautySum(self, s: str) -> int:
+        total_beauty = 0
+        for i in range(len(s)):
+            freq = Counter()
+            for j in range(i,len(s)):
+                freq[s[j]] += 1
+                total_beauty += max(freq.values()) - min(freq.values())
+        return total_beauty
