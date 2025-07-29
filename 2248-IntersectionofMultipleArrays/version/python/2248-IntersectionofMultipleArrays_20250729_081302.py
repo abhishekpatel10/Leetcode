c5b845036fdcf3b075af206c7f4de3a1a48d941a
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-07-29 08:13:03 -0500

    Create 2248-IntersectionofMultipleArrays

---
+++ 2248-IntersectionofMultipleArrays/version/python/2248-IntersectionofMultipleArrays_20250729_081302.py
@@ -0,0 +1,17 @@
+# Last updated: 7/29/2025, 8:13:02 AM
+from collections import Counter
+from typing import List
+
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        b = []
+        a = Counter()
+
+        for i in range(len(nums)):
+            a.update(set(nums[i]))  # use set to avoid duplicate counts from same list
+
+        for key, value in a.items():
+            if value == len(nums):
+                b.append(key)  # fixed from `values` to `key`
+
+        return sorted(b)
