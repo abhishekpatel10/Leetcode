0dc8cab55f17ca7fd36b51b6789c19eeea03eae6
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-08-08 14:19:58 -0500

    Create 808-SoupServings

---
+++ 808-SoupServings/version/python/808-SoupServings_20250808_141959.py
@@ -0,0 +1,30 @@
+# Last updated: 8/8/2025, 2:19:59 PM
+class Solution:
+    def soupServings(self, n: int) -> float:
+        units = math.ceil(n / 25)
+        if units >= 200:
+            return 1.0
+
+        dp = [[-1.0] * (units + 1) for _ in range(units + 1)]
+        ops = [(4, 0), (3, 1), (2, 2), (1, 3)]
+
+        def prob(a: int, b: int) -> float:
+            if a <= 0 and b <= 0:
+                return 0.5
+            if a <= 0:
+                return 1.0
+            if b <= 0:
+                return 0.0
+            if dp[a][b] != -1.0:   
+                return dp[a][b]
+
+            res = 0.0
+            for da, db in ops:
+                res += 0.25 * prob(max(0, a - da), max(0, b - db))
+
+            dp[a][b] = res
+            return res
+
+        return prob(units, units)
+        
+        
