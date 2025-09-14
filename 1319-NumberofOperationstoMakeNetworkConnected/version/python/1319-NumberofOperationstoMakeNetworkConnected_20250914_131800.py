d9fb02961f5f3f8ed495ce97407526fe6efe55e6
abhishekpatel10 <85981289+abhishekpatel10@users.noreply.github.com>
2025-09-14 13:17:59 -0400

    Create 1319-NumberofOperationstoMakeNetworkConnected

---
+++ 1319-NumberofOperationstoMakeNetworkConnected/version/python/1319-NumberofOperationstoMakeNetworkConnected_20250914_131800.py
@@ -0,0 +1,36 @@
+# Last updated: 9/14/2025, 1:18:00 PM
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n - 1:
+            return -1 
+        rank = [0] * n
+        par = list(range(n))
+
+        def findPar(x):
+            if par[x] != x:
+                par[x] = findPar(par[x])
+            return par[x]
+        
+        def Union(y,z):
+            root_y = findPar(y)
+            root_z = findPar(z)
+            if root_y == root_z:
+                return 
+            if rank[root_y] < rank[root_z]:
+                par[root_y] = root_z
+            elif rank[root_y] > rank[root_z]:
+                par[root_z] = root_y
+            else:
+                par[root_z] = root_y
+                rank[root_y] += 1
+        for u, v in connections:
+            Union(u, v)
+        
+        
+        components = 0
+        for i in range(n):
+            if findPar(i) == i:
+                components += 1
+        
+        
+        return components - 1
