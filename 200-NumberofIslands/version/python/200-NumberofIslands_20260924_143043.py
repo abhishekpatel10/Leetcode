bf6971a4626445e638e37044ef55d56cc34ade0f
Abhishek Hiteshbhai Patel <85981289+abhishekpatel10@users.noreply.github.com>
2026-09-24 14:30:43 -0500

    Create 200-NumberofIslands

---
+++ 200-NumberofIslands/version/python/200-NumberofIslands_20260924_143043.py
@@ -0,0 +1,26 @@
+# Last updated: 9/24/2026, 2:30:43 PM
+1class Solution:
+2    def numIslands(self, grid: List[List[str]]) -> int:
+3        n = len(grid)
+4        m = len(grid[0])
+5        vis = [[0] * m for _ in range(n)]
+6        
+7        ans = 0
+8        for i in range(n):
+9            for j in range(m):
+10                if vis[i][j] == 0 and grid[i][j] == "1":
+11                    ans += 1
+12                    self.dfs(i,j,grid,vis,n,m)
+13        return ans
+14    def dfs(self,i,j,grid,vis,n,m):
+15        vis[i][j] = 1
+16        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+17  
+18        for dr, dc in directions:
+19            nr = i + dr
+20            nc = j + dc
+21            if ( 0 <= nr < n and 0 <= nc < m and grid[nr][nc] == "1" and vis[nr][nc] == 0):
+22                vis[nr][nc] = 1
+23                self.dfs(nr,nc,grid,vis,n,m)
+24        
+25
